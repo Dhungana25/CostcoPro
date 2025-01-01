ef90637d406a031f2475805c6a247f2c25ad2dbe
Dhungana25 <sarmila.mehta73@gmail.com>
2024-12-31 21:34:15 -0600

    changes made

--- src/main/java/search/Search_Product.java
+++ src/main/java/search/Search_Product.java
@@ -6,6 +6,8 @@ public class Search_Product {
 
 	public static void main(String[] args) {
 		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
 		
 
 	}
