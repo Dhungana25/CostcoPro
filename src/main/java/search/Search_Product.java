76b5db1b35d4fabcad5043d732a57283e72e5bcc
Dhungana25 <sarmila.mehta73@gmail.com>
2025-01-02 13:06:11 -0600

    changes made

--- src/main/java/search/Search_Product.java
+++ src/main/java/search/Search_Product.java
@@ -1,5 +1,8 @@
 package search;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Search_Product {
@@ -8,6 +11,10 @@ public class Search_Product {
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com/");
 		driver.manage().window().maximize();
+		WebElement searchtf= driver.findElement(By.xpath("//textarea[@id='APjFqb']"));
+		searchtf.sendKeys("swizerland");
+		searchtf.sendKeys(Keys.ENTER);
+		
 		
 
 	}
