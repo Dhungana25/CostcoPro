5b3a9bb4891fb80563cc90e63ff8cce24da6090b
Dhungana25 <sarmila.mehta73@gmail.com>
2025-01-03 14:47:01 -0600

    new changes

--- src/main/java/search/Search_Product.java
+++ src/main/java/search/Search_Product.java
@@ -1,19 +1,25 @@
 package search;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Search_Product {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		ChromeDriver driver = new ChromeDriver();
-		driver.get("https://www.google.com/");
+		driver.get("https://www.costco.com/");
 		driver.manage().window().maximize();
-		WebElement searchtf= driver.findElement(By.xpath("//textarea[@id='APjFqb']"));
-		searchtf.sendKeys("swizerland");
+		WebElement searchtf= driver.findElement(By.xpath("//input[@id=':r1:']"));
+		searchtf.sendKeys("sofa");
 		searchtf.sendKeys(Keys.ENTER);
+		((JavascriptExecutor)driver).executeScript("scroll(0,300)");
+		Thread.sleep(3000);
+		WebElement secondsofa= driver.findElement(By.xpath("(//a[@class='MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineHover mui-8ujno3'])[2]"));
+		secondsofa.click();
+		
 		
 		
 
